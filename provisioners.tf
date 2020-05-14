d1229fad7573691663ce619ea24a268f1ec58696
Ronald Konjer <ronaldkonjer@gmail.com>
2020-05-14 21:44:40 +0200

    only one bastion

--- provisioners.tf
+++ provisioners.tf
@@ -3,13 +3,14 @@
  */
 
 resource "null_resource" "setup-bastion" {
-  count = local.az_count
+  count    = 1
   triggers = {
     bastion_id  = element(aws_instance.bastion.*.id, count.index)
     auth_keys   = md5(local.authorized_keys)
     hostname_id = md5(join(",", module.bastion-hostname.resource_ids))
   }
   connection {
+    type        = "ssh"
     host        = element(data.aws_eip.bastion.*.public_ip, count.index)
     user        = var.bastion_user
     private_key = local.private_key
