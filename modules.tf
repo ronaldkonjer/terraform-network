aade22df55f04c5f74e21d7736cc1bfaeb4a1053
Ronald Konjer <ronaldkonjer@gmail.com>
2020-05-14 11:36:26 +0200

    fix count error

--- modules.tf
+++ modules.tf
@@ -4,7 +4,7 @@
 
 module "bastion-hostname" {
   source           = "github.com/ronaldkonjer/terraform-hostname.git"
-  count            = local.az_count
+  hcount            = local.az_count
   fqdns            = aws_route53_record.bastion.*.fqdn
   addresses        = data.aws_eip.bastion.*.public_ip
   user             = var.bastion_user
